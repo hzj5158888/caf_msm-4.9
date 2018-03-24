5f0d5dcb6aa91adfd8445adf428b95a1ab9c0f83
Demon Singur <demonsingur@gmail.com>
2018-03-24 17:27:51 +0000

    smb-lib: add charging current limitting
    Charging at 3200mA while using the phone makes it quite hot.
    Decrease the charging current to ~1600mA to prevent bad user
    experience and complaints.
    
    After 90 seconds with screen on, vote USB ICL to lower current
    to 1600mA.
    After 5 seconds with screen off, unvote USB ICL so the current
    goes back to 3200mA.
    
    Also, add compatibility with stock hvdcp service that writes to
    the system_temp_level property to trigger thermal throttling.
    
    Values taken from MIUI kernel, rewritten to look better and avoid
    locking in framebuffer notifiers.
    
    Change-Id: Iedf2789304356221283472ba242211d3ea6256d8

--- drivers/power/supply/qcom/smb-lib.h
+++ drivers/power/supply/qcom/smb-lib.h
@@ -72,6 +72,10 @@ enum print_reason {
 #define HVDCP2_ICL_VOTER		"HVDCP2_ICL_VOTER"
 #define OV_VOTER			"OV_VOTER"
 
+#ifdef CONFIG_MACH_XIAOMI_MSM8998
+#define FB_SCREEN_VOTER			"FB_SCREEN_VOTER"
+#endif
+
 #define VCONN_MAX_ATTEMPTS	3
 #define OTG_MAX_ATTEMPTS	3
 #define BOOST_BACK_STORM_COUNT	3
@@ -270,6 +274,9 @@ struct smb_charger {
 
 	/* notifiers */
 	struct notifier_block	nb;
+#ifdef CONFIG_MACH_XIAOMI_MSM8998
+	struct notifier_block	fb_state_notifier;
+#endif
 
 	/* parallel charging */
 	struct parallel_params	pl;
@@ -314,6 +321,9 @@ struct smb_charger {
 	struct work_struct	legacy_detection_work;
 	struct delayed_work	uusb_otg_work;
 	struct delayed_work	bb_removal_work;
+#ifdef CONFIG_MACH_XIAOMI_MSM8998
+	struct delayed_work	fb_state_work;
+#endif
 
 	/* cached status */
 	int			voltage_min_uv;
@@ -323,7 +333,13 @@ struct smb_charger {
 	int			boost_threshold_ua;
 	int			system_temp_level;
 	int			thermal_levels;
+#ifdef CONFIG_MACH_XIAOMI_MSM8998
+	int			*thermal_mitigation_dcp;
+	int			*thermal_mitigation_qc3;
+	int			*thermal_mitigation_qc2;
+#else
 	int			*thermal_mitigation;
+#endif
 	int			dcp_icl_ua;
 	int			fake_capacity;
 	int			fake_batt_status;
@@ -353,6 +369,9 @@ struct smb_charger {
 	bool			use_extcon;
 	bool			otg_present;
 	bool			is_audio_adapter;
+#ifdef CONFIG_MACH_XIAOMI_MSM8998
+	bool			screen_on;
+#endif
 
 	/* workaround flag */
 	u32			wa_flags;
